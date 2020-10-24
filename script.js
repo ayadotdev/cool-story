a54c28591a1c8c6c552d615569ea4a53ced82b51
Teiko <teikomancini1993@gmail.com>
2020-10-24 12:42:09 +0200

    wrote choice select js and html for 4 choices

--- script.js
+++ script.js
@@ -29,28 +29,180 @@ function pressBtn2(){
 function pressBtn3(){
     document.getElementById("btn-3").style.display ="none"
     document.getElementById("btn-4").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-7").style.display="block"
+    document.getElementById("btn-8").style.display="block"
     document.getElementById("text4").style.display="block"
 }
 function pressBtn4(){
     document.getElementById("btn-3").style.display ="none"
     document.getElementById("btn-4").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-9").style.display="block"
+    document.getElementById("btn-10").style.display="block"
     document.getElementById("text5").style.display="block"
 }
 function pressBtn5(){
     document.getElementById("btn-5").style.display ="none"
     document.getElementById("btn-6").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-11").style.display="block"
+    document.getElementById("btn-12").style.display="block"
     document.getElementById("text6").style.display="block"
 }
 function pressBtn6(){
     document.getElementById("btn-5").style.display ="none"
     document.getElementById("btn-6").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-13").style.display="block"
+    document.getElementById("btn-14").style.display="block"
     document.getElementById("text7").style.display="block"
+}
+function pressBtn7(){
+    document.getElementById("btn-7").style.display ="none"
+    document.getElementById("btn-8").style.display ="none"
+    document.getElementById("btn-15").style.display="block"
+    document.getElementById("btn-16").style.display="block"
+    document.getElementById("text8").style.display="block"
+}
+function pressBtn8(){
+    document.getElementById("btn-7").style.display ="none"
+    document.getElementById("btn-8").style.display ="none"
+    document.getElementById("btn-17").style.display="block"
+    document.getElementById("btn-18").style.display="block"
+    document.getElementById("text9").style.display="block"
+}
+function pressBtn9(){
+    document.getElementById("btn-9").style.display ="none"
+    document.getElementById("btn-10").style.display ="none"
+    document.getElementById("btn-19").style.display="block"
+    document.getElementById("btn-20").style.display="block"
+    document.getElementById("text10").style.display="block"
+}
+function pressBtn10(){
+    document.getElementById("btn-11").style.display ="none"
+    document.getElementById("btn-12").style.display ="none"
+    document.getElementById("btn-21").style.display="block"
+    document.getElementById("btn-22").style.display="block"
+    document.getElementById("text11").style.display="block"
+}
+function pressBtn11(){
+    document.getElementById("btn-11").style.display ="none"
+    document.getElementById("btn-12").style.display ="none"
+    document.getElementById("btn-23").style.display="block"
+    document.getElementById("btn-24").style.display="block"
+    document.getElementById("text12").style.display="block"
+}
+function pressBtn12(){
+    document.getElementById("btn-11").style.display ="none"
+    document.getElementById("btn-12").style.display ="none"
+    document.getElementById("btn-25").style.display="block"
+    document.getElementById("btn-26").style.display="block"
+    document.getElementById("text13").style.display="block"
+}
+function pressBtn13(){
+    document.getElementById("btn-13").style.display ="none"
+    document.getElementById("btn-14").style.display ="none"
+    document.getElementById("btn-27").style.display="block"
+    document.getElementById("btn-28").style.display="block"
+    document.getElementById("text14").style.display="block"
+}
+function pressBtn14(){
+    document.getElementById("btn-13").style.display ="none"
+    document.getElementById("btn-14").style.display ="none"
+    document.getElementById("btn-29").style.display="block"
+    document.getElementById("btn-30").style.display="block"
+    document.getElementById("text15").style.display="block"
+}
+function pressBtn15(){
+    document.getElementById("btn-15").style.display ="none"
+    document.getElementById("btn-16").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text16").style.display="block"
+}
+function pressBtn16(){
+    document.getElementById("btn-15").style.display ="none"
+    document.getElementById("btn-16").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text17").style.display="block"
+}
+function pressBtn17(){
+    document.getElementById("btn-17").style.display ="none"
+    document.getElementById("btn-18").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text18").style.display="block"
+}
+function pressBtn18(){
+    document.getElementById("btn-17").style.display ="none"
+    document.getElementById("btn-18").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text19").style.display="block"
+}
+function pressBtn19(){
+    document.getElementById("btn-19").style.display ="none"
+    document.getElementById("btn-20").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text20").style.display="block"
+}
+function pressBtn20(){
+    document.getElementById("btn-19").style.display ="none"
+    document.getElementById("btn-20").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text21").style.display="block"
+}
+function pressBtn21(){
+    document.getElementById("btn-21").style.display ="none"
+    document.getElementById("btn-22").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text22").style.display="block"
+}
+function pressBtn22(){
+    document.getElementById("btn-21").style.display ="none"
+    document.getElementById("btn-22").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text23").style.display="block"
+}
+function pressBtn23(){
+    document.getElementById("btn-23").style.display ="none"
+    document.getElementById("btn-24").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text24").style.display="block"
+}
+function pressBtn24(){
+    document.getElementById("btn-23").style.display ="none"
+    document.getElementById("btn-24").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text25").style.display="block"
+}
+function pressBtn25(){
+    document.getElementById("btn-25").style.display ="none"
+    document.getElementById("btn-26").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text26").style.display="block"
+}
+function pressBtn26(){
+    document.getElementById("btn-25").style.display ="none"
+    document.getElementById("btn-26").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text27").style.display="block"
+}
+function pressBtn27(){
+    document.getElementById("btn-27").style.display ="none"
+    document.getElementById("btn-28").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text28").style.display="block"
+}
+function pressBtn28(){
+    document.getElementById("btn-27").style.display ="none"
+    document.getElementById("btn-28").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text29").style.display="block"
+}
+function pressBtn29(){
+    document.getElementById("btn-29").style.display ="none"
+    document.getElementById("btn-30").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text30").style.display="block"
+}
+function pressBtn30(){
+    document.getElementById("btn-29").style.display ="none"
+    document.getElementById("btn-30").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text31").style.display="block"
 }
