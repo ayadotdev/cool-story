476a28996965b0b5bd89dd31f2ea8a76079cee1a
Teiko <teikomancini1993@gmail.com>
2020-10-24 11:23:48 +0200

    created choice script

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+function hideChoiceButton() {
+    document.getElementById("btn-1").style.display ="none"
+}
+
+function createNewButton() {
+    document.getElementById("btn-1").style.display="block"
+}
+
+function textAppear() {
+    document.getElementById("first-part").style.display="block"
+}
+*/
+
+function pressBtn1(){
+    document.getElementById("btn-1").style.display ="none"
+    document.getElementById("btn-2").style.display ="none"
+    document.getElementById("btn-3").style.display="block"
+    document.getElementById("btn-4").style.display="block"
+    document.getElementById("text2").style.display="block"
+}
+function pressBtn2(){
+    document.getElementById("btn-1").style.display ="none"
+    document.getElementById("btn-2").style.display ="none"
+    document.getElementById("btn-5").style.display="block"
+    document.getElementById("btn-6").style.display="block"
+    document.getElementById("text3").style.display="block"
+}
+function pressBtn3(){
+    document.getElementById("btn-3").style.display ="none"
+    document.getElementById("btn-4").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text4").style.display="block"
+}
+function pressBtn4(){
+    document.getElementById("btn-3").style.display ="none"
+    document.getElementById("btn-4").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text5").style.display="block"
+}
+function pressBtn5(){
+    document.getElementById("btn-5").style.display ="none"
+    document.getElementById("btn-6").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text6").style.display="block"
+}
+function pressBtn6(){
+    document.getElementById("btn-5").style.display ="none"
+    document.getElementById("btn-6").style.display ="none"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("btn-share").style.display="block"
+    document.getElementById("text7").style.display="block"
+}
