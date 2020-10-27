eb80a45dc73c00960d4449b964c8b7df81bb8f10
Teiko <teikomancini1993@gmail.com>
2020-10-27 10:40:35 +0100

    started merging

--- script.js
+++ script.js
@@ -1,208 +1,105 @@
-/*
-function hideChoiceButton() {
-    document.getElementById("btn-1").style.display ="none"
+
+//----------------CHAPTER STRUCTURE
+
+const chapter5 = {
+    phrase: "You survived!!!",
+    img: "https://www.colemanconcierge.com/wp-content/uploads/2017/11/Ghosts-and-Legends-You-Survived.jpg",
+    buttonChoice1: { text: "", nextChapter: null },
+    buttonChoice2: { text: "", nextChapter: null }
 }
 
-function createNewButton() {
-    document.getElementById("btn-1").style.display="block"
+const chapter4 = {
+    phrase: "You died!!!",
+    img: "https://i.kym-cdn.com/entries/icons/facebook/000/029/198/Dark_Souls_You_Died_Screen_-_Completely_Black_Screen_0-2_screenshot.jpg",
+    buttonChoice1: { text: "", nextChapter: null },
+    buttonChoice2: { text: "", nextChapter: null }
 }
 
-function textAppear() {
-    document.getElementById("first-part").style.display="block"
+const chapter3 = {
+    phrase: "The ghost is here right in front of you! You have to choose!",
+    img: "https://dnbstories.com/wp-content/uploads/2016/09/Ghosts.jpg",
+    buttonChoice1: { text: "Run!", nextChapter: chapter5 },
+    buttonChoice2: { text: "Fight!", nextChapter: chapter4 }
 }
-*/
 
-function pressBtn1(){
-    document.getElementById("btn-1").style.display ="none"
-    document.getElementById("btn-2").style.display ="none"
-    document.getElementById("btn-3").style.display="block"
-    document.getElementById("btn-4").style.display="block"
-    document.getElementById("text2").style.display="block"
+const chapter2 = {
+    phrase: "You entered the room and you see the ghost. What do you do?",
+    img: "https://images.unsplash.com/photo-1505599950318-db5a5093ce0c?ixlib=rb-1.2.1&q=50&fm=jpg&crop=entropy&cs=tinysrgb&w=720&fit=max&ixid=eyJhcHBfaWQiOjEyMjZ9",
+    buttonChoice1: { text: "Get closer", nextChapter: chapter3 },
+    buttonChoice2: { text: "Hide", nextChapter: chapter5 }
 }
-function pressBtn2(){
-    document.getElementById("btn-1").style.display ="none"
-    document.getElementById("btn-2").style.display ="none"
-    document.getElementById("btn-5").style.display="block"
-    document.getElementById("btn-6").style.display="block"
-    document.getElementById("text3").style.display="block"
+
+const chapter1 = {
+    phrase: "You wake up in the very dark and cold room. You open your eyes and you see the door. Would you open that door?",
+    img: "https://theroyalliar.files.wordpress.com/2019/05/sv2t3.jpg?w=640",
+    buttonChoice1: { text: "Yes", nextChapter: chapter2 },
+    buttonChoice2: { text: "No", nextChapter: chapter5 }
 }
-function pressBtn3(){
-    document.getElementById("btn-3").style.display ="none"
-    document.getElementById("btn-4").style.display ="none"
-    document.getElementById("btn-7").style.display="block"
-    document.getElementById("btn-8").style.display="block"
-    document.getElementById("text4").style.display="block"
-}
-function pressBtn4(){
-    document.getElementById("btn-3").style.display ="none"
-    document.getElementById("btn-4").style.display ="none"
-    document.getElementById("btn-9").style.display="block"
-    document.getElementById("btn-10").style.display="block"
-    document.getElementById("text5").style.display="block"
-}
-function pressBtn5(){
-    document.getElementById("btn-5").style.display ="none"
-    document.getElementById("btn-6").style.display ="none"
-    document.getElementById("btn-11").style.display="block"
-    document.getElementById("btn-12").style.display="block"
-    document.getElementById("text6").style.display="block"
-}
-function pressBtn6(){
-    document.getElementById("btn-5").style.display ="none"
-    document.getElementById("btn-6").style.display ="none"
-    document.getElementById("btn-13").style.display="block"
-    document.getElementById("btn-14").style.display="block"
-    document.getElementById("text7").style.display="block"
-}
-function pressBtn7(){
-    document.getElementById("btn-7").style.display ="none"
-    document.getElementById("btn-8").style.display ="none"
-    document.getElementById("btn-15").style.display="block"
-    document.getElementById("btn-16").style.display="block"
-    document.getElementById("text8").style.display="block"
-}
-function pressBtn8(){
-    document.getElementById("btn-7").style.display ="none"
-    document.getElementById("btn-8").style.display ="none"
-    document.getElementById("btn-17").style.display="block"
-    document.getElementById("btn-18").style.display="block"
-    document.getElementById("text9").style.display="block"
-}
-function pressBtn9(){
-    document.getElementById("btn-9").style.display ="none"
-    document.getElementById("btn-10").style.display ="none"
-    document.getElementById("btn-19").style.display="block"
-    document.getElementById("btn-20").style.display="block"
-    document.getElementById("text10").style.display="block"
-}
-function pressBtn10(){
-    document.getElementById("btn-11").style.display ="none"
-    document.getElementById("btn-12").style.display ="none"
-    document.getElementById("btn-21").style.display="block"
-    document.getElementById("btn-22").style.display="block"
-    document.getElementById("text11").style.display="block"
-}
-function pressBtn11(){
-    document.getElementById("btn-11").style.display ="none"
-    document.getElementById("btn-12").style.display ="none"
-    document.getElementById("btn-23").style.display="block"
-    document.getElementById("btn-24").style.display="block"
-    document.getElementById("text12").style.display="block"
-}
-function pressBtn12(){
-    document.getElementById("btn-11").style.display ="none"
-    document.getElementById("btn-12").style.display ="none"
-    document.getElementById("btn-25").style.display="block"
-    document.getElementById("btn-26").style.display="block"
-    document.getElementById("text13").style.display="block"
-}
-function pressBtn13(){
-    document.getElementById("btn-13").style.display ="none"
-    document.getElementById("btn-14").style.display ="none"
-    document.getElementById("btn-27").style.display="block"
-    document.getElementById("btn-28").style.display="block"
-    document.getElementById("text14").style.display="block"
-}
-function pressBtn14(){
-    document.getElementById("btn-13").style.display ="none"
-    document.getElementById("btn-14").style.display ="none"
-    document.getElementById("btn-29").style.display="block"
-    document.getElementById("btn-30").style.display="block"
-    document.getElementById("text15").style.display="block"
-}
-function pressBtn15(){
-    document.getElementById("btn-15").style.display ="none"
-    document.getElementById("btn-16").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text16").style.display="block"
-}
-function pressBtn16(){
-    document.getElementById("btn-15").style.display ="none"
-    document.getElementById("btn-16").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text17").style.display="block"
-}
-function pressBtn17(){
-    document.getElementById("btn-17").style.display ="none"
-    document.getElementById("btn-18").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text18").style.display="block"
-}
-function pressBtn18(){
-    document.getElementById("btn-17").style.display ="none"
-    document.getElementById("btn-18").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text19").style.display="block"
-}
-function pressBtn19(){
-    document.getElementById("btn-19").style.display ="none"
-    document.getElementById("btn-20").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text20").style.display="block"
-}
-function pressBtn20(){
-    document.getElementById("btn-19").style.display ="none"
-    document.getElementById("btn-20").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text21").style.display="block"
-}
-function pressBtn21(){
-    document.getElementById("btn-21").style.display ="none"
-    document.getElementById("btn-22").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text22").style.display="block"
-}
-function pressBtn22(){
-    document.getElementById("btn-21").style.display ="none"
-    document.getElementById("btn-22").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text23").style.display="block"
-}
-function pressBtn23(){
-    document.getElementById("btn-23").style.display ="none"
-    document.getElementById("btn-24").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text24").style.display="block"
-}
-function pressBtn24(){
-    document.getElementById("btn-23").style.display ="none"
-    document.getElementById("btn-24").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text25").style.display="block"
-}
-function pressBtn25(){
-    document.getElementById("btn-25").style.display ="none"
-    document.getElementById("btn-26").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text26").style.display="block"
-}
-function pressBtn26(){
-    document.getElementById("btn-25").style.display ="none"
-    document.getElementById("btn-26").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text27").style.display="block"
-}
-function pressBtn27(){
-    document.getElementById("btn-27").style.display ="none"
-    document.getElementById("btn-28").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text28").style.display="block"
-}
-function pressBtn28(){
-    document.getElementById("btn-27").style.display ="none"
-    document.getElementById("btn-28").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text29").style.display="block"
-}
-function pressBtn29(){
-    document.getElementById("btn-29").style.display ="none"
-    document.getElementById("btn-30").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text30").style.display="block"
-}
-function pressBtn30(){
-    document.getElementById("btn-29").style.display ="none"
-    document.getElementById("btn-30").style.display ="none"
-    document.getElementById("btn-share").style.display="block"
-    document.getElementById("text31").style.display="block"
+
+
+
+
+
+//----------------LOGIC
+
+// This function has only the logic to display the chapter in the HTML
+// (phrase and img in the #wholestory div  and the buttons in the #buttons div )
+function changeChapter(chapterToShow) {
+
+    //remove all existing buttons from the #buttons div
+    document.querySelector("#buttons").innerHTML = "";
+    //append a new <p> to the #wholestory div 
+    const wholestory = document.querySelector("#wholestory");
+
+    const paragraph = document.createElement("p");
+    const text = document.createTextNode(chapterToShow.phrase);
+    paragraph.appendChild(text);
+    wholestory.appendChild(paragraph);
+
+    const img = document.createElement("img");
+    img.src = chapterToShow.img;
+    img.width = "600";
+    img.height = "400";
+
+    wholestory.appendChild(img);
+
+
+
+
+
+    //SCROLL TO BOTTOM
+    wholestory.scrollTop = wholestory.scrollHeight;
+
+
+
+
+    //if there is a nextChapter in buttonChoice1 we add the button 
+    if (chapterToShow.buttonChoice1.nextChapter !== null) {
+
+        const button1 = document.createElement("input");
+        button1.type = "button";
+        button1.value = chapterToShow.buttonChoice1.text;
+        button1.addEventListener("click", function () {
+            changeChapter(chapterToShow.buttonChoice1.nextChapter);
+        }, false);
+
+        document.querySelector("#buttons").appendChild(button1);
+    }
+
+    //if there is a nextChapter in buttonChoice2 we add the button 
+    if (chapterToShow.buttonChoice2.nextChapter !== null) {
+
+
+        const button2 = document.createElement("input");
+        button2.type = "button";
+        button2.value = chapterToShow.buttonChoice2.text;
+        button2.addEventListener("click", function () {
+            changeChapter(chapterToShow.buttonChoice2.nextChapter);
+        }, false);
+
+        document.querySelector("#buttons").appendChild(button2);
+    }
+
+
+
 }
